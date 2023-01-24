91587e290cf1ff0f3a9b6bb43595f49953ae7c44
almazgaliev <almazgaliev99@gmail.com>
2023-01-24 21:07:41 +0300

    fix: small bug

--- src/asteroids.js
+++ src/asteroids.js
@@ -6,7 +6,7 @@ export class AsteroidField {
     for (let ix = 0; ix < amount; ix++) {
       this.coords.push([]);
       for (let i = 0; i < 16; i++) {
-        let phi = (i * 2 * Math.PI) / n;
+        let phi = (i * 2 * Math.PI) / 16;
         let r = Math.random() * 10 + 30;
         this.coords[ix].push([Math.cos(phi) * r, -Math.sin(phi) * r, 1]);
       };
