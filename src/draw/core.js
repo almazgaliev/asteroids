f57c0fea6f338c1c732447a608bfdb859725c638
almazgaliev <almazgaliev99@gmail.com>
2023-02-18 11:48:29 +0300

    wip: added strokeCicrle function

--- src/draw/core.js
+++ src/draw/core.js
@@ -10,6 +10,13 @@ export function fillCircle(ctx, x, y, r) {
   ctx.fill();
 }
 
+export function strokeCircle(ctx, x, y, r) {
+  ctx.beginPath();
+  ctx.arc(x, y, r, 0, 2 * Math.PI);
+  ctx.closePath();
+  ctx.stroke();
+}
+
 export function strokePolygon(ctx, coords) {
   ctx.beginPath();
   ctx.moveTo(coords[0][0], coords[0][1]);
